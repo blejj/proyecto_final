cdcb0b708770517b17d86eb7e2af525338b3bc5c
blejj <fbleileee@gmail.com>
2024-10-25 22:29:58 -0300

    Se agregó la carpeta de android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC-Kql5LiMxa7a7B2KmYUfC2D1p9gq-JyM',
+    authDomain: 'proyectfinal-8aefc.firebaseapp.com',
+    projectId: 'proyectfinal-8aefc',
+    storageBucket: 'proyectfinal-8aefc.appspot.com',
+    messagingSenderId: '501220010978',
+    appId: '1:501220010978:web:354614c564d7a7f85527bb',
+  },
 };
